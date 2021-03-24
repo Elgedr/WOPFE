23d621517f4f39bfa266c9aaae814a2b2488a825
Anton-7 <antmat@ttu.ee>
2021-03-24 10:24:44 +0200

    issue60: events instead of timetable

--- src/main/java/eu/judegam/wopfe/models/repositories/person/student/StudentService.java
+++ src/main/java/eu/judegam/wopfe/models/repositories/person/student/StudentService.java
@@ -1,5 +1,6 @@
 package eu.judegam.wopfe.models.repositories.person.student;
 
+import eu.judegam.wopfe.models.school.tests.Test;
 import eu.judegam.wopfe.models.timetable.Event;
 import eu.judegam.wopfe.models.timetable.Timetable;
 import eu.judegam.wopfe.models.user.Student;
@@ -24,8 +25,19 @@ public class StudentService {
         }
         Timetable tt = new Timetable("testTimetable", new ArrayList<>());
         Event e1 = new Event("testEvent", "testPlace", "testTeacher", tt);
+        e1.setDay("Monday");
+        e1.setTime("08:00");
         tt.getEvents().add(e1);
         return tt;
+    }
+
+    public Test getStudentTasksById(Long id) {
+//        Optional<Student> foundStud = repo.findById(id);
+//        if (foundStud.isPresent()) {
+//            return foundStud.get().getTask();
+//        }
+        Test test = new Test("testTimetable", "now", 123);
+        return test;
     }
 
 }
